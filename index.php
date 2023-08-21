ee98f4df79549d885ef5373a189653065193a404
marcocampisi <marco.campisi@outlook.it>
2023-08-21 17:16:25 +0200

    Add base files

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Censuratore</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<form action="badwords.php" method="post">
+    <p>Paragrafo: <textarea name="paragraph"></textarea></p>
+</form>
+
+</body>
+</html>
