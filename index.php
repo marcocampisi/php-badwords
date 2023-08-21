f96bb9015009ec0f489db251197129c122c4008e
marcocampisi <marco.campisi@outlook.it>
2023-08-21 17:24:16 +0200

    Add submit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,11 +12,15 @@
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
-<form action="badwords.php" method="post">
-    <p>Paragrafo: <textarea name="paragraph"></textarea></p>
-</form>
+
+    <form action="badwords.php" method="post">
+        <p>Paragrafo: <textarea name="paragraph"></textarea></p>
+        <p>Parola da censurare: <input type="text" name="word"></p>
+        <input type="submit" value="Submit">
+    </form>
 
 </body>
+
 </html>
